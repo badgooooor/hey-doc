7cff4717980fcb5d3cbd2fa53607c9c4bf7c18fb
Yuttakhan B <yuttakhanbaingen@gmail.com>
2023-07-28 17:15:40 +0700

    :sparkles: Apply to page

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import { onDestroy, onMount } from "svelte";
+  import { onDestroy } from "svelte";
   import { chatLogStore } from "./stores";
+  import Recording from "./components/Recording.svelte";
 
   type DisplayChatLog = {
     role: string;
@@ -9,33 +10,6 @@
   
   let message = ''
   let chatLogs: DisplayChatLog[] = [];
-
-  let media: Blob[] = [];
-  let mediaRecorder: MediaRecorder | null = null;
-  onMount(async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-		
-		mediaRecorder = new MediaRecorder(stream);
-		mediaRecorder.ondataavailable = (e) => media.push(e.data)
-		mediaRecorder.onstop = function(){
-			const audio = document.querySelector('audio');
-			const blob = new Blob(media, {'type' : 'audio/ogg; codecs=opus' });
-			media = [];
-      console.log(media)
-			audio.src = window.URL.createObjectURL(blob);
-		}
-  })
-
-  function startRecording(){
-    console.log('starttt')
-		mediaRecorder.start();
-	}
-
-	function stopRecording(){
-    console.log('stoppp')
-
-		mediaRecorder.stop();
-	}
 
   const unsubscribeChatLogStore = chatLogStore.subscribe(val => chatLogs = val);
   onDestroy(unsubscribeChatLogStore);
@@ -63,9 +37,7 @@
       </div>
       <div class="">
         <div>Speech to text</div>
-        <audio controls />
-        <button on:click={startRecording}>Record</button>
-        <button on:click={stopRecording}>Stop</button>
+        <Recording />
       </div>
       <div class="flex items-center">
         <input 
